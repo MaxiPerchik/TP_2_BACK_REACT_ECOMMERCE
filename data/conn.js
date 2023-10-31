8dae5d7b15e77702e3459efe3cc614bac45d7314
Maxi <mperchik@protonmail.com>
2023-10-30 21:13:12 -0300

    + dotenv para no publicar la URL de mongo a todo el mundo

--- data/conn.js
+++ data/conn.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:admin@cluster0.llj8x8c.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
